80645ed49fddd1decf108505f309c876565c887d
Lucas Pessoa <lcppcoding@gmail.com>
2021-07-12 10:58:45 -0300

    reducer added

--- src/Application.js
+++ src/Application.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useReducer, useCallback } from 'react';
 
 import id from 'uuid/v4';
 
@@ -7,23 +7,45 @@ import NewGrudge from './NewGrudge';
 
 import initialState from './initialState';
 
-const Application = () => {
-  const [grudges, setGrudges] = useState(initialState);
+const GRUDGE_ADD = 'GRUDGE_ADD'
+const GRUDGE_FORGIVE = 'GRUDGE_FORGIVE'
 
-  const addGrudge = grudge => {
-    grudge.id = id();
-    grudge.forgiven = false;
-    setGrudges([grudge, ...grudges]);
-  };
-
-  const toggleForgiveness = id => {
-    setGrudges(
-      grudges.map(grudge => {
-        if (grudge.id !== id) return grudge;
-        return { ...grudge, forgiven: !grudge.forgiven };
+const reducer = (state, action) => {
+  console.log({ action })
+  switch(action.type) {
+    case GRUDGE_ADD:
+      return [action.payload, ...state]
+    case GRUDGE_FORGIVE:
+      return state.map(grudge => {
+        if (grudge.id !== action.payload.id) return grudge
+        return {...grudge, forgiven: !grudge.forgiven }
       })
-    );
-  };
+    default:
+      return state
+  }
+}
+
+const Application = () => {
+  const [grudges, dispatch] = useReducer(reducer, initialState);
+
+  const addGrudge = useCallback(({ person, reason }) => {
+    dispatch({
+      type: GRUDGE_ADD,
+      payload: {
+        person,
+        reason,
+        forgiven: false,
+        id: id()
+      }
+    })
+  }, [dispatch]);
+
+  const toggleForgiveness = useCallback(id => {
+    dispatch({
+      type: GRUDGE_FORGIVE,
+      payload: { id }
+    })
+  }, [dispatch]);
 
   return (
     <div className="Application">
--- src/NewGrudge.js
+++ src/NewGrudge.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 
-const NewGrudge = ({ onSubmit }) => {
+const NewGrudge = React.memo(({ onSubmit }) => {
   const [person, setPerson] = useState('');
   const [reason, setReason] = useState('');
+
+  console.log('Rendering New Grudge')
 
   const handleChange = event => {
     event.preventDefault();
@@ -28,6 +30,6 @@ const NewGrudge = ({ onSubmit }) => {
       <input className="NewGrudge-submit button" type="submit" />
     </form>
   );
-};
+});
 
 export default NewGrudge;
